4dab0130acc085587b078bae863b100e41644702
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-10 10:20:11 +0530

    Adding the 2nd view of the poll-app.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,3 +7,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world,you are in polls index")
+
+
+def detail(request,question_id):
+    response = "you are looking at the result of the question %s."
+    return HttpResponse(response % question_id)
