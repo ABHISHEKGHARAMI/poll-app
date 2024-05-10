439d60f0271e656e19b0f787632b01ce7bdc20e4
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-10 11:35:44 +0530

    mod .

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,9 +4,13 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Questions
+
 
 def index(request):
-    return HttpResponse("Hello world,you are in polls index")
+    latest_question_list = Questions.objects.order_by('-pub_date')[:5]
+    output = "".join([q.question_text for q in latest_question_list] )
+    return HttpResponse(output)
 
 
 def detail(request,question_id):
