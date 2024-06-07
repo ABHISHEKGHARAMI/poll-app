17158730702b9fbda5725f439f4895cae28373c4
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-07 12:22:38 +0530

    .mod

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,34 +12,24 @@ from django.urls import reverse
 from . import views
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-   
+# using the generic views for the django
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
     
-    context = {
-        'latest_question_list' : latest_question_list
-    }
+    def query_set(self):
+        return Question.objects.order_by('-pub_date')[:5]
     
-    return render(request,'polls/index.html',context)
-
-
-def detail(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/detail.html',{
-        'question' : question
-    })
-
-
-def results(request,question_id):
-    questions  = get_object_or_404(Question,pk = question_id)
-    return render(
-        request,
-        "polls/result.html",
-        {
-            'questions': questions
-        }
-    )
-
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    
+    
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "polls/result.html"
+    
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
