d27796b0f6521cc9b13af5269231335fa937ac01
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-07 21:28:31 +0530

    writing my first test case .

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+import datetime
+from django.utils import timezone
+
+
+from .models import Question
+
+# class for the test
+class QuestionModelTests(TestCase):
+    def test_was_published_with_future_questions(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_questions = Question(pub_date=time)
+        self.assertIs(future_questions.was_published_recently(),False)
