1ee48b7df78003e689abce181c287501384b5521
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-07 11:08:19 +0530

    .mod

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -24,7 +24,7 @@ def index(request):
 
 
 def detail(request,question_id):
-    question = get_object_or_404(Questions,pk=question_id)
+    question = get_object_or_404(Question,pk=question_id)
     return render(request,'polls/detail.html',{
         'question' : question
     })
@@ -42,7 +42,7 @@ def results(request,question_id):
 
 
 def vote(request,question_id):
-    questions = get_object_or_404(Questions,pk=question_id)
+    question = get_object_or_404(Question,pk=question_id)
     try:
         selected_choice = questions.choice_set.get(pk = request.POST['choice'])
         print(selected_choice)
@@ -51,7 +51,7 @@ def vote(request,question_id):
             request,
             "polls/detail.html",
             {
-                'questions' : questions,
+                'question' : question,
                 'error_message' : "you didn't select a choice" 
             }
         )
@@ -60,4 +60,4 @@ def vote(request,question_id):
         selected_choice.votes = F('votes') + 1
         selected_choice.save()
         
-        return HttpResponseRedirect(reverse("polls:result", args=(questions.id,)))
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
