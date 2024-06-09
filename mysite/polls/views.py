99718fe1870c3ca6be9801e1463f3309f7cb4f79
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-09 19:39:18 +0530

    Adding more views for the timezone .

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,6 +10,7 @@ from django.template import loader
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from . import views
+from django.utils import timezone
 
 
 # using the generic views for the django
@@ -18,7 +19,7 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
     
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
     
     
 class DetailView(generic.DetailView):
