76caed95c3f4b37a1a0a84d8a32928f24a1bf521
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-10 12:09:30 +0530

    .mod

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,4 +4,8 @@ from django.contrib import admin
 from .models import Question
 
 
+# building the model for the admin site
+class QuestionAdmin(admin.modelAdmin):
+    fields = ['pub_date','question_text']
+
 admin.site.register(Question)
